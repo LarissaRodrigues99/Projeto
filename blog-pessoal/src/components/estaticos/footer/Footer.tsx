1d9f13f6b01698cb56a46abe59d6f7d8903ff633
Larissa <larissa989130904@gmail.com>
2022-09-30 18:55:19 -0300

    Foi atualizado os códigos das pastas Home,Navbar e Footer

--- blog-pessoal/src/components/estaticos/footer/Footer.tsx
+++ blog-pessoal/src/components/estaticos/footer/Footer.tsx
@@ -1,28 +1,30 @@
+import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
+import "./Footer.css";
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import React from "react";
+
 
 function Footer () {
     return(
         <>
         <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box display="flex" flexDirection="column" alignItems="center" className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="textos">Siga-nos nas redes sociais </Typography>
                         </Box>
                             <a href="https://www.linkedin.com/in/larissa-rodrigues-b44869181/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 35, color: "white" }} />
+                                <LinkedInIcon className="redes"/>
                             </a>
                         </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className="box2">
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className="textos" >© 2022 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" gutterBottom className="textos" align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -2,44 +2,36 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import {Box} from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar () {
     return(
         <>
         <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
-                        </Typography>
+                    <Box className="cursor" >
+                        <Typography variant="h5" color="inherit"> Blog Pessoal </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit"> Tela Inicial </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit"> Postagens </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit"> Temas </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit"> Cadastrar Tema </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                logout
-                            </Typography>
-                        </Box>
+                        <Link to="/login" className="text-decorator-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="primary" > Logout </Typography>
+                            </Box>
+                        </Link>
+                        
                     </Box>
 
                 </Toolbar>
