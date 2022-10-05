e640e0f509bb194f838a03c9869240a1e6da1b8e
Larissa <larissa989130904@gmail.com>
2022-10-05 14:28:49 -0300

    Foi adicionado link do Swagger

--- blog-pessoal/src/services/Service.ts
+++ blog-pessoal/src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create ({
 
-    baseURL:"https://blogpessoalbackendgen.herokuapp.com/swagger-ui/index.html"
+    baseURL:"https://blogdothiagofaccipieri.onrender.com/"
     
 })
 
