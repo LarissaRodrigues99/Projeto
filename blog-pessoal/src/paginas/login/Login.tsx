d57e9eeb66d02fe4b08b41501c8d79f3903da8b6
Larissa <larissa989130904@gmail.com>
2022-10-04 21:48:38 -0300

    Atualizando informação

--- blog-pessoal/src/paginas/login/Login.tsx
+++ blog-pessoal/src/paginas/login/Login.tsx
@@ -1,31 +1,74 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { Button, Grid, TextField, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import { login} from "../../services/Service";
 import { Box } from "@mui/material";
-import React from "react";
 import "./Login.css";
+import UserLogin from "../../models/UserLogin";
 
 function Login(){
+
+        let history = useNavigate();
+    const [token, setToken] = useLocalStorage("token");
+    const [userLogin, setUserLogin]= useState<UserLogin>(
+        {
+            id: 0,
+            usuario: " ",
+            senha: " ",
+            token: ""
+        }
+    )
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+        useEffect(()=>{
+            if(token!=""){
+              //  history.push("/home")
+            }
+        }, [token])
+        
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+                e.preventDefault();
+                try{
+                        await login(`/usuarios/logar`, userLogin, setToken)
+                        alert("Usuário logado com sucesso!");
+                }
+                catch(erro){
+                    alert ("Dados do usuário inconsistentes. Erro ao logar!");
+
+                }
+        }
+
     return(
                 <Grid container direction="row" justifyContent="center" alignItems="center">
                     <Grid alignItems="center" xs={6}>
                         <Box paddingX={20}>
-                            <form>
+                            <form onSubmit={onSubmit}>
                                 <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos"> Entrar </Typography>
-                                <TextField id="usuário" label="usuário" variant="outlined" name="usuário" margin="normal" fullWidth />
-                                <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
+                                <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}  id="usuário" label="usuário" variant="outlined" name="usuário" margin="normal" fullWidth />
+                                <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
                                 <Box marginTop={2} textAlign="center">
-                                    <Link to="/home" className="text-decorator-none">
                                         <Button type="submit" variant="contained" color="primary">
                                             Logar
                                         </Button>
-                                    </Link>
+                                
                                 </Box>
                             </form>
                                 <Box display="flex" justifyContent="center" marginTop={2}>
                                     <Box>
                                         <Typography variant="subtitle1" gutterBottom align="center">Não tem conta? </Typography>
                                     </Box>
+                                    <Link to = "/cadastrousuario">
                                     <Typography variant="subtitle1" gutterBottom align="center" style={{fontWeight:"bold"}}> cadastre-se </Typography>
+                                    </Link>
+                                    
                                 </Box>
                         </Box>
                     </Grid>
