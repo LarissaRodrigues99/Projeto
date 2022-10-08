4783e9884f89e12a57ccfbd0c8e0589457db1ca9
Larissa <larissa989130904@gmail.com>
2022-10-07 21:46:11 -0300

    Foi adicionado uma className na linha 43

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -2,9 +2,20 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import {Box} from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 
 function Navbar () {
+
+    const [token,setToken] = useLocalStorage("token")
+    let navgate=useNavigate();
+
+    function goLogout(){
+        setToken("")
+        alert("Usuário deslogado")
+        navgate("/login")
+    }
+
     return(
         <>
         <AppBar position="static">
@@ -14,23 +25,31 @@ function Navbar () {
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit"> Tela Inicial </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit"> Postagens </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit"> Temas </Typography>
-                        </Box>
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit"> Tela Inicial </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/postagem" className="text-decorator-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit"> Postagens </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit"> Temas </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/fotmularioTema" className="text-decorator-none">
                         <Box mx={1} className="cursor">
                             <Typography variant="h6" color="inherit"> Cadastrar Tema </Typography>
                         </Box>
-                        <Link to="/login" className="text-decorator-none">
-                            <Box mx={1} className="cursor">
-                                <Typography variant="h6" color="primary" > Logout </Typography>
-                            </Box>
                         </Link>
+                        
+                        <Box mx={1} className="cursor" onClick={goLogout}>
+                            <Typography variant="h6" color="inherit" > Logout </Typography>
+                        </Box>
+                        
                         
                     </Box>
 
